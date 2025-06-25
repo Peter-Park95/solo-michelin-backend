e5c46b9cdf25165b3d2386a824a20cd97e0481c6
황인규 <hwang0707@douzone.com>
2025-06-25 19:33:53 +0900

    이미지 삭제 기능 추가

--- src/main/java/com/michelin/config/WebConfig.java
+++ src/main/java/com/michelin/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // 모든 API 경로
                         .allowedOrigins("http://localhost:5173") // Vite dev 서버 주소
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowCredentials(true);
             }
         };
